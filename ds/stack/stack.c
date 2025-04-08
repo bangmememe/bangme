487b4ac6af93a9d6b1d0fdbf381efa7d389cd506
bangme <qkrgkdbf10@gmail.com>
2025-04-08 12:33:48 +0900

    fuck

---
+++ ds/stack/stack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int stack[100];
+int tos;          // top of stack
+
+
+void push(int data)
+{
+
+
+}
+int pop(void)
+{
+
+}
+
+int main(void)
+{
+    push(100);
+    push(200);
+    push(300);
+
+    int re = pop(); printf("1st pop() : %d\n", re);
+    re = pop();     printf("2st pop() : %d\n", re);
+    re = pop();     printf("3st pop() : %d\n", re);
+
+    return 0;
+}
