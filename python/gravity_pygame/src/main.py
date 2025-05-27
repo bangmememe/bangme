52f9cda060f111e3685f2735d7d35b93be13789d
bangmememe <qkrgkdbf10@gmail.com>
2025-05-27 09:15:02 +0900

    씨발 집갈래

---
+++ python/gravity_pygame/src/main.py
@@ -0,0 +1,10 @@
+from gravity import Game
+
+def main():
+    print("Sylveon")
+    game = Game()
+    game.run()
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ python/poetry_test/test_poetry.py
@@ -0,0 +1,11 @@
+import requests
+import numpy
+
+def main():
+    print("poetry 소스 테스트")
+    print(requests.__version__)
+    print(numpy.__version__)
+    
+    
+if __name__ == "__main__":
+    main()
